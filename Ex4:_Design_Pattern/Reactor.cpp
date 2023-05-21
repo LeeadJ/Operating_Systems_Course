370a9f26585e9ca2c7ce66231f87bc10c33af456
Leead Jacobowitz <leead123@gmail.com>
2023-05-21 23:27:13 +0300

    added create, start,  stop functions

--- Ex4:_Design_Pattern/Reactor.cpp
+++ Ex4:_Design_Pattern/Reactor.cpp
@@ -16,3 +16,26 @@ Reactor::~Reactor()
         close(fd);
     }
 }
+
+// Creating the Reactor
+void* Reactor::createReactor() 
+{
+    return static_cast<void*>(this);
+}
+
+// Starting the Reactor
+void Reactor::startReactor() 
+{
+    int result = pthread_create(&reactorThread, nullptr, &Reactor::reactorLoop, this);
+    if(result != 0)
+    {
+        perror("--ERROR-- pthread_creat failed (in -> startReactor())");
+    }
+}
+
+// Stoping the Reactor
+void Reactor::stopReactor() {
+    shouldStop = true;
+    // wait for the thread to complete before returning
+    pthread_join(reactorThread, nullptr);
+}
