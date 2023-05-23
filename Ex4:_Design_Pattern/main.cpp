2452bc3d36d2818b6f1b10b4ccf61ebe60b9325b
Leead Jacobowitz <leead123@gmail.com>
2023-05-23 11:41:46 +0300

    finished main

--- Ex4:_Design_Pattern/main.cpp
+++ Ex4:_Design_Pattern/main.cpp
@@ -1,6 +1,16 @@
 #include "Server.hpp"
-
-int main()
+#include "Reactor.hpp"
+int main() 
 {
+    Server server;
+
+    try 
+    {
+        server.start();
+    } catch (const std::exception& e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
